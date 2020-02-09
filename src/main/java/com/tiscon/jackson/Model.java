983819f7cfed0bc6662e3dcbb2b6f667861c0823
izm51 <tatsuhiko.imaizumi051@gmail.com>
2020-02-09 10:19:07 +0900

    add: getDirectionDistance()

---
+++ src/main/java/com/tiscon/jackson/Model.java
@@ -0,0 +1,41 @@
+
+package com.tiscon.jackson;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "geocoded_waypoints",
+    "routes",
+    "status"
+})
+public class Model {
+
+    @JsonProperty("geocoded_waypoints")
+    public List<GeocodedWaypoint> geocodedWaypoints = null;
+    @JsonProperty("routes")
+    public List<Route> routes = null;
+    @JsonProperty("status")
+    public String status;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
